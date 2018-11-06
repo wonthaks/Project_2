f2a9352f4c2950d78bafd778ecdd72081d14d1a2
Wontha <wontha.ks@gmail.com>
2018-11-05 21:01:26 -0500

    Code added to label exit and to read in up to 4 characters for a string from user input

--- project2.s
+++ project2.s
@@ -3,6 +3,9 @@
 .text
 main:
    li $v0, 8		#load 8 in $v0 to read string input from user
+   la $a0, buffer	#load buffer into $a0
+   li $a1, 5		#5 in $a1 to store 5 bytes in buffer
+   syscall			
+exit:
    li $v0, 10		#to end the script
-   
    syscall
