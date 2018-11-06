314ed5d2a9f9e417c2998cbf51c71dfa540cd2d8
Wontha <wontha.ks@gmail.com>
2018-11-05 21:07:16 -0500

    Code added to store values in multiple registers to use in main loop

--- project2.s
+++ project2.s
@@ -5,7 +5,14 @@ main:
    li $v0, 8		#load 8 in $v0 to read string input from user
    la $a0, buffer	#load buffer into $a0
    li $a1, 5		#5 in $a1 to store 5 bytes in buffer
-   syscall			
+   syscall		
+
+   add $t0, $a0, $zero	#copy contents of $a0 to $t0
+   li $t1, 1			#load 1 into $t1 to use as program counter
+   li $t8, 5			#load 5 into $t8 to use to check whether loop has eded
+   li $t7, 10			#load 10 into $t7 to use to check for Line Feed character
+   
 exit:
    li $v0, 10		#to end the script
    syscall
+   
